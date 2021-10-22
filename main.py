849f1d3b0bbf01f20aaa6da1ac294753813fb5f7
Tomasz Lemke <s19471@pjwstk.edu.pl>
2021-10-22 16:14:30 +0200

    finished docstrings

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax, SSS
+"""
+Authors: Krzysztof Skwira & Tomasz Lemke
+"""
 
 
 class ConnectFour(TwoPlayerGame):
@@ -15,35 +18,36 @@ class ConnectFour(TwoPlayerGame):
         self.current_player = 1  # player 1 starts.
 
     def possible_moves(self):
+        """Repeat the draw if invalid column selected"""
         return [i for i in range(7) if (self.board[:, i].min() == 0)]
 
     def make_move(self, column):
+        """Adding piece to selected column"""
         line = np.argmin(self.board[:, column] != 0)
         self.board[line, column] = self.current_player
 
     def show(self):
+        """Showing board with current pieces played"""
         print('\n' + '\n'.join(
             ['0 1 2 3 4 5 6', 13 * '-'] +
             [' '.join([['.', 'O', 'X'][self.board[5 - j][i]]
                        for i in range(7)]) for j in range(6)]))
 
     def lose(self):
+        """Checking if opponent has won"""
         return find_four(self.board, self.opponent_index)
 
     def is_over(self):
-        """
-        Checking if the game ended either by lack of moves or winning condition met
-        """
+        """Checking if the game ended either by lack of moves or winning condition met"""
         return (self.board.min() > 0) or self.lose()
 
     def scoring(self):
+        """Simple logic to prevent the algorithm to select a losing move if other options are available"""
         return -100 if self.lose() else 0
 
 
 def find_four(board, current_player):
-    """
-    Returns True if the player has connected 4 (or more)
-    """
+    """Returns True if the player has connected 4 (or more) in a line"""
     for pos, direction in POS_DIR:
         streak = 0
         while (0 <= pos[0] <= 5) and (0 <= pos[1] <= 6):
---
+++ readme.md
@@ -0,0 +1,25 @@
+<h3>
+Authors: Krzysztof Skwira & Tomasz Lemke
+</h3>
+
+Classic Game of Connect4 utilizing easyAI library. \
+Game can be played between a Human Player and AI or between two AIs.\
+AI are using Negamax and SSS algorithms to determine the next move. \
+By default both AIs are calculating 5 moves in advance.
+
+Enjoy!
+
+<h3>
+Installation: 
+</h3>
+
+pip install easyAI \
+pip install numpy
+
+<h3>
+Reference:
+</h3>
+
+https://en.wikipedia.org/wiki/Negamax  \
+https://en.wikipedia.org/wiki/SSS*  \
+https://zulko.github.io/easyAI/
